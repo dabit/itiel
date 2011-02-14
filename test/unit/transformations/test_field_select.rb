59cd52f76b134b6a2337671b7da308b06cf73aa7
David Padilla <david@crowdint.com>
2011-02-14 12:24:10 -0800

    Test file for future FieldSelect transformation

---
+++ test/unit/transformations/test_field_select.rb
@@ -0,0 +1,24 @@
+require File.expand_path("#{File.dirname(__FILE__)}/../../test_helper.rb")
+
+describe Itiel::Transformations::FieldSelect do
+  before :each do
+    @transformer = Itiel::Transformations::FieldSelect.new("order_id", "name")
+    @transformer.input = [
+        {"order_id" => 1, "name" => "bucket", "price" => 1.5},
+        {"order_id" => 2, "name" => "second bucket", "price" => 3}
+    ]
+
+    @transformer.transform!
+
+    @result = @transformer.output
+  end
+
+  it "returns only specified fields" do
+    expected_result = [
+        {"order_id" => 1, "name" => "bucket"},
+        {"order_id" => 2, "name" => "second bucket"}
+    ]
+
+    assert_equal expected_result, @result
+  end
+end
