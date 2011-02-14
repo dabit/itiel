5f92c9eb282f8ce6137ada9b85a64face1048d87
David Padilla <david@crowdint.com>
2011-02-13 17:48:57 -0800

    Refactored the class variables to allow defaults

--- lib/itiel/source.rb
+++ lib/itiel/source.rb
@@ -1,7 +1,11 @@
 module Itiel
   class Source
-    class << self
-      attr_accessor :config_file_path
+    def self.config_file_path
+      @@config_file_path ||= 'config'
+    end
+
+    def self.config_file_path=(value)
+      @@config_file_path = value
     end
   end
 end
--- test/unit/test_source.rb
+++ test/unit/test_source.rb
@@ -2,6 +2,15 @@ require File.expand_path("#{File.dirname(__FILE__)}/../test_helper")
 
 describe Itiel::Source do
   it "responds to :config_file_path" do
-    assert_equal true, Itiel::Source.respond_to?(:config_file_path)
+    assert_equal Itiel::Source.respond_to?(:config_file_path), true
+  end
+
+  it "has a default value of config" do
+    assert_equal Itiel::Source.config_file_path, 'config'
+  end
+
+  it "stores the new value on config_file_path" do
+    Itiel::Source.config_file_path = 'new_path'
+    assert_equal Itiel::Source.config_file_path, 'new_path'
   end
 end
