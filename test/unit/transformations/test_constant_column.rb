ee2cded94783d07a7bff658a6d581b2f1ea38de8
David Padilla <david@crowdint.com>
2011-02-14 20:38:25 -0800

    Add Constant column test

---
+++ test/unit/transformations/test_constant_column.rb
@@ -0,0 +1,33 @@
+require File.expand_path("#{File.dirname(__FILE__)}/../../test_helper.rb")
+
+describe Itiel::Transformations::ConstantField do
+  before :each do
+    @input = [
+      {
+        "id" => "1"
+      },
+      {
+        "id" => "2"
+      }
+    ]
+
+    @expected_output = [
+      {
+        "id"     => "1",
+        "append" => "value"
+      },
+      {
+        "id"     => "2",
+        "append" => "value"
+      }
+    ]
+
+    @transformation       = Itiel::Transformations::ConstantField("append" => "value")
+    @transformation.input = @input
+    @result               = @transformations.output
+  end
+
+  it "appends a field with a constant value to the stream" do
+    assert_equal @expected_output, @result
+  end
+end
