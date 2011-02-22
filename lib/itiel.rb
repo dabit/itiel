c821957a070d4b65fb4e725ae5d42540d2112a20
David Padilla <david@crowdint.com>
2011-02-22 10:13:37 -0800

    Made the load file more readable

--- lib/itiel.rb
+++ lib/itiel.rb
@@ -8,24 +8,24 @@ module Itiel
   autoload :Nameable               , 'itiel/nameable'
 
   module Extractors
-    autoload :CSVFile             , 'itiel/extractors/csv_file'
-    autoload :DatabaseConnection  , 'itiel/extractors/database_connection'
-    autoload :DatabaseTable       , 'itiel/extractors/database_table'
-    autoload :InputOutputBehavior , 'itiel/extractors/input_output_behavior'
+    autoload :CSVFile              , 'itiel/extractors/csv_file'
+    autoload :DatabaseConnection   , 'itiel/extractors/database_connection'
+    autoload :DatabaseTable        , 'itiel/extractors/database_table'
+    autoload :InputOutputBehavior  , 'itiel/extractors/input_output_behavior'
   end
 
   module Loaders
-    autoload :CSVFile             , 'itiel/loaders/csv_file'
-    autoload :DatabaseTable       , 'itiel/loaders/database_table'
-    autoload :InputOutputBehavior , 'itiel/loaders/input_output_behavior'
+    autoload :CSVFile              , 'itiel/loaders/csv_file'
+    autoload :DatabaseTable        , 'itiel/loaders/database_table'
+    autoload :InputOutputBehavior  , 'itiel/loaders/input_output_behavior'
   end
 
   module Transformations
-    autoload :CalculatedField       , 'itiel/transformations/calculated_field'
-    autoload :ConstantField       , 'itiel/transformations/constant_field'
-    autoload :FieldRename         , 'itiel/transformations/field_rename'
-    autoload :FieldSelect         , 'itiel/transformations/field_select'
-    autoload :InputOutputBehavior , 'itiel/transformations/input_output_behavior.rb'
-    autoload :SingleColumnSort    , 'itiel/transformations/single_column_sort.rb'
+    autoload :CalculatedField      , 'itiel/transformations/calculated_field'
+    autoload :ConstantField        , 'itiel/transformations/constant_field'
+    autoload :FieldRename          , 'itiel/transformations/field_rename'
+    autoload :FieldSelect          , 'itiel/transformations/field_select'
+    autoload :InputOutputBehavior  , 'itiel/transformations/input_output_behavior.rb'
+    autoload :SingleColumnSort     , 'itiel/transformations/single_column_sort.rb'
   end
 end
