353c795c04f2358f94f5c1768cbcf7ce29a4fb2a
David Padilla <david@crowdint.com>
2013-03-15 13:14:42 -0600

    Simplify the creation of the blogpost table

--- features/step_definitions/extractor/database_steps.rb
+++ features/step_definitions/extractor/database_steps.rb
@@ -1,19 +1,13 @@
-Given /^a blogpost database table with rows:$/ do |table|
-  Itiel::Extract::CustomSQL.connection_file_path = @database_config_path
+Given /^an? (.+) database table with rows:$/ do |table_name, table|
+  step "a #{table_name} database table"
 
-  Itiel::Extract::CustomSQL.sequel_connection(:test).create_table :blogposts do
-    Integer :id
-    String :title
-    String :author
-  end
-
-  blogposts = Itiel::Extract::CustomSQL.sequel_connection(:test)[:blogposts]
+  rows = Itiel::Extract::CustomSQL.sequel_connection(:test)[:blogposts]
   table.hashes.each do |row|
-    blogposts.insert row
+    rows.insert row
   end
 end
 
-Given /^a blogpost database table$/ do
+Given /^an? blogpost database table$/ do
   Itiel::Extract::CustomSQL.connection_file_path = @database_config_path
 
   Itiel::Extract::CustomSQL.sequel_connection(:test).create_table :blogposts do
